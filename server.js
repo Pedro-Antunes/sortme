f798727b57103c01c5f09398f0a8e257b0a07459
Pedro-Antunes <antunes2011p@gmail.com>
2019-06-18 13:02:18 +0100

    forth commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+
+var port = process.env.PORT || 3000;
+app.use(express.static('html'));
+
+app.get('/', function(req, res){
+    res.sendFile('html/begin.html',{root: __dirname})
+});
+
+server.listen(port, '0.0.0.0',function(){
+    console.log('OLAAAAA, ESTOU A FUNCIONAR POR AGORA');
+});
